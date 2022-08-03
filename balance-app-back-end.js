7b24e8465046d4dac262ddee3743ebf92992f410
EddieJorden <eddie@eddiejorden.tech>
2022-08-03 15:28:10 -0700

    fixed networking

--- balance-app-back-end.js
+++ balance-app-back-end.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const HOST = '0.0.0.0'
 
 app.get('/', (req, res) => {
 	res.send('Hello World sah');
 })
 
 app.listen(port, () => {
-	console.log(`listening on port ${port}`);
+	console.log(`listening at ${HOST}:${port}`);
 })
